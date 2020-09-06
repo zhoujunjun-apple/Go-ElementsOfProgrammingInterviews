8a5b42750a14f4b6caf497b58ba1c509454981d4
unknown <nujnujouhz@foxmail.com>
2020-09-06 22:39:15 +0800

    [add] add DropLowestSetParity solution, test and benchmark

--- chapter_05/5.1_ComputingTheParityOfAWord/main.go
+++ chapter_05/5.1_ComputingTheParityOfAWord/main.go
@@ -1,27 +1,40 @@
 package main
 
-//BruteForceParity function iteratively check each bit of word
+//BruteForceParity function iteratively check each bit of word. O(N) time. N is the word bit length
 func BruteForceParity(word int8) int8 {
 	var oneCount int8 = 0
 	var one int8 = 1
 	for word > 0 {
-		oneCount += word & one  // "word & 1" extract the lowest bit from word
-		word = word>>1
+		oneCount += word & one // "word & 1" extract the lowest bit from word
+		word = word >> 1
 	}
-	return oneCount%2
+	return oneCount % 2
 }
 
-//BruteForceXORParity function iteratively check each bit of word with XOR operation
+//BruteForceXORParity function iteratively check each bit of word with XOR operation. O(N) time
 func BruteForceXORParity(word int8) int8 {
 	var oneCount int8 = 0
 	var one int8 = 1
 
 	for word > 0 {
-		oneCount ^= word & one  //use XOR to count
-		word = word>>1
+		oneCount ^= word & one //use XOR to count
+		word = word >> 1
 	}
 
 	return oneCount
 }
 
-func main(){}
+//DropLowestSetParity function drops the lowest set bit of word iteratively. Worst O(N) time
+func DropLowestSetParity(word int8) int8 {
+	var oneCount int8 = 0
+	var one int8 = 1
+
+	for word > 0 {
+		oneCount ^= one
+		word &= (word - 1) // this operation drops lowest set bit of word
+	}
+
+	return oneCount
+}
+
+func main() {}
--- chapter_05/5.1_ComputingTheParityOfAWord/main_test.go
+++ chapter_05/5.1_ComputingTheParityOfAWord/main_test.go
@@ -4,25 +4,25 @@ import (
 	"testing"
 )
 
-func Test_BruteForceParity(t *testing.T){
-	tests := []struct{
-		word int8
+func Test_BruteForceParity(t *testing.T) {
+	tests := []struct {
+		word     int8
 		expected int8
 	}{
 		{
-			word: 1,
+			word:     1,
 			expected: 1,
 		},
 		{
-			word: 2,
+			word:     2,
 			expected: 1,
 		},
 		{
-			word: 3,
+			word:     3,
 			expected: 0,
 		},
 		{
-			word: 127,
+			word:     127,
 			expected: 1,
 		},
 	}
@@ -35,25 +35,25 @@ func Test_BruteForceParity(t *testing.T){
 	}
 }
 
-func Test_BruteForceXORParity(t *testing.T){
-	tests := []struct{
-		word int8
+func Test_BruteForceXORParity(t *testing.T) {
+	tests := []struct {
+		word     int8
 		expected int8
 	}{
 		{
-			word: 1,
+			word:     1,
 			expected: 1,
 		},
 		{
-			word: 2,
+			word:     2,
 			expected: 1,
 		},
 		{
-			word: 3,
+			word:     3,
 			expected: 0,
 		},
 		{
-			word: 127,
+			word:     127,
 			expected: 1,
 		},
 	}
@@ -63,5 +63,50 @@ func Test_BruteForceXORParity(t *testing.T){
 		if ret != tt.expected {
 			t.Errorf("expected=%d, got=%d", tt.expected, ret)
 		}
+	}
+}
+
+func Test_DropLowestSetParity(t *testing.T) {
+	tests := []struct {
+		word     int8
+		expected int8
+	}{
+		{
+			word:     1,
+			expected: 1,
+		},
+		{
+			word:     2,
+			expected: 1,
+		},
+		{
+			word:     3,
+			expected: 0,
+		},
+		{
+			word:     127,
+			expected: 1,
+		},
+	}
+
+	for _, tt := range tests {
+		ret := DropLowestSetParity(tt.word)
+		if ret != tt.expected {
+			t.Errorf("expected=%d, got=%d", tt.expected, ret)
+		}
+	}
+}
+
+func Benchmark_DropLowestSetParity(t *testing.B) {
+	var word int8 = 112
+	for i := 0; i < t.N; i++ {
+		DropLowestSetParity(word)
+	}
+}
+
+func Benchmark_BruteForceXORParity(t *testing.B) {
+	var word int8 = 112
+	for i := 0; i < t.N; i++ {
+		BruteForceXORParity(word)
 	}
 }
