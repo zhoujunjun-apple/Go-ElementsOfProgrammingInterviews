17d62ad2e944daf5f1c928d1527b1b86935eefb6
unknown <nujnujouhz@foxmail.com>
2020-09-06 21:23:24 +0800

    [add] add IntToStr solution and test

--- chapter_07/7.1_InterconvertStringsAndIntegers/main.go
+++ chapter_07/7.1_InterconvertStringsAndIntegers/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 //NativeStrToInt function convert string to integer
 func NativeStrToInt(intStr string) int {
 	intNeg := false
@@ -61,6 +66,54 @@ func SubstractStrToInt(intStr string) int {
 	return intVal
 }
 
-func main() {
+var DigitToString = map[int]string{
+	0: "0",
+	1: "1",
+	2: "2",
+	3: "3",
+	4: "4",
+	5: "5",
+	6: "6",
+	7: "7",
+	8: "8",
+	9: "9",
+}
 
+//IntToStr function convert integer into string
+func IntToStr(it int) string {
+	rs := []string{}
+
+	itNeg := false
+	if it < 0 {
+		itNeg = true
+		it = -it
+	}
+
+	remain := it
+	for it >= 10 {
+		remain = it % 10
+		if rv, ok := DigitToString[remain]; ok {
+			rs = append(rs, rv)
+		}
+		it = it / 10
+		fmt.Printf("it=%d, remain=%d, rs=%v\n", it, remain, rs)
+	}
+	if rv, ok := DigitToString[it]; ok {
+		rs = append(rs, rv)
+	}
+	if itNeg {
+		rs = append(rs, "-")
+	}
+
+	for left, right := 0, len(rs)-1; left < right; {
+		rs[left], rs[right] = rs[right], rs[left]
+		left++
+		right--
+	}
+
+	return strings.Join(rs, "")
+}
+
+func main() {
+	fmt.Println(IntToStr(-123))
 }
--- chapter_07/7.1_InterconvertStringsAndIntegers/main_test.go
+++ chapter_07/7.1_InterconvertStringsAndIntegers/main_test.go
@@ -57,3 +57,34 @@ func Test_SubstractStrToInt(t *testing.T) {
 		}
 	}
 }
+
+func Test_IntToStr(t *testing.T) {
+	tests := []struct {
+		integer  int
+		expected string
+	}{
+		{
+			expected: "321",
+			integer:  321,
+		},
+		{
+			expected: "-124",
+			integer:  -124,
+		},
+		{
+			expected: "-1",
+			integer:  -1,
+		},
+		{
+			expected: "0",
+			integer:  0,
+		},
+	}
+
+	for _, tt := range tests {
+		ret := IntToStr(tt.integer)
+		if ret != tt.expected {
+			t.Errorf("expected=%s, got=%s", tt.expected, ret)
+		}
+	}
+}
