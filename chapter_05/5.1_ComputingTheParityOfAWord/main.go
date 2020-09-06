18f66c913efda71b06040e2d0e9d83c2452c2c36
unknown <nujnujouhz@foxmail.com>
2020-09-06 22:14:45 +0800

    [add] add BruteForceXORParity solution and test

--- chapter_05/5.1_ComputingTheParityOfAWord/main.go
+++ chapter_05/5.1_ComputingTheParityOfAWord/main.go
@@ -5,10 +5,23 @@ func BruteForceParity(word int8) int8 {
 	var oneCount int8 = 0
 	var one int8 = 1
 	for word > 0 {
-		oneCount += word & one
+		oneCount += word & one  // "word & 1" extract the lowest bit from word
 		word = word>>1
 	}
 	return oneCount%2
+}
+
+//BruteForceXORParity function iteratively check each bit of word with XOR operation
+func BruteForceXORParity(word int8) int8 {
+	var oneCount int8 = 0
+	var one int8 = 1
+
+	for word > 0 {
+		oneCount ^= word & one  //use XOR to count
+		word = word>>1
+	}
+
+	return oneCount
 }
 
 func main(){}
--- chapter_05/5.1_ComputingTheParityOfAWord/main_test.go
+++ chapter_05/5.1_ComputingTheParityOfAWord/main_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func Test_BruteForce(t *testing.T){
+func Test_BruteForceParity(t *testing.T){
 	tests := []struct{
 		word int8
 		expected int8
@@ -29,6 +29,37 @@ func Test_BruteForce(t *testing.T){
 
 	for _, tt := range tests {
 		ret := BruteForceParity(tt.word)
+		if ret != tt.expected {
+			t.Errorf("expected=%d, got=%d", tt.expected, ret)
+		}
+	}
+}
+
+func Test_BruteForceXORParity(t *testing.T){
+	tests := []struct{
+		word int8
+		expected int8
+	}{
+		{
+			word: 1,
+			expected: 1,
+		},
+		{
+			word: 2,
+			expected: 1,
+		},
+		{
+			word: 3,
+			expected: 0,
+		},
+		{
+			word: 127,
+			expected: 1,
+		},
+	}
+
+	for _, tt := range tests {
+		ret := BruteForceXORParity(tt.word)
 		if ret != tt.expected {
 			t.Errorf("expected=%d, got=%d", tt.expected, ret)
 		}
