a88bb977efa759454bd236b8eb2d61ed7fa8959e
unknown <nujnujouhz@foxmail.com>
2020-09-06 22:02:40 +0800

    [add] add BruteForceParity solution and test

--- chapter_05/5.1_ComputingTheParityOfAWord/main.go
+++ chapter_05/5.1_ComputingTheParityOfAWord/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main(){
-	
+//BruteForceParity function iteratively check each bit of word
+func BruteForceParity(word int8) int8 {
+	var oneCount int8 = 0
+	var one int8 = 1
+	for word > 0 {
+		oneCount += word & one
+		word = word>>1
+	}
+	return oneCount%2
 }
+
+func main(){}
---
+++ chapter_05/5.1_ComputingTheParityOfAWord/main_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_BruteForce(t *testing.T){
+	tests := []struct{
+		word int8
+		expected int8
+	}{
+		{
+			word: 1,
+			expected: 1,
+		},
+		{
+			word: 2,
+			expected: 1,
+		},
+		{
+			word: 3,
+			expected: 0,
+		},
+		{
+			word: 127,
+			expected: 1,
+		},
+	}
+
+	for _, tt := range tests {
+		ret := BruteForceParity(tt.word)
+		if ret != tt.expected {
+			t.Errorf("expected=%d, got=%d", tt.expected, ret)
+		}
+	}
+}
