2a52b24ab91dd3adfbbd322ae333d14a38656fe2
unknown <nujnujouhz@foxmail.com>
2020-09-20 13:02:16 +0800

    [add] add FindNonKBalancedNode function and test cases

--- chapter_10/10.1_TestIfABinaryTreeIsHeight-Balanced/variant/FindNotK-BalancedNode/main.go
+++ chapter_10/10.1_TestIfABinaryTreeIsHeight-Balanced/variant/FindNotK-BalancedNode/main.go
@@ -2,6 +2,7 @@ package main
 
 //TreeNode is a tree node
 type TreeNode struct {
+	name  string
 	left  *TreeNode
 	right *TreeNode
 }
@@ -9,17 +10,46 @@ type TreeNode struct {
 //NodeInfo is the recored information in FindNonKBalancedNode
 type NodeInfo struct {
 	node *TreeNode
+	size int
 }
 
 //FindNonKBalancedNode funtion returns a node in root tree that
 //not k-balanced, but all of its descendants are k-balanced.
+//O(N) time and O(height of tree) space
 func FindNonKBalancedNode(root *TreeNode, k int) NodeInfo {
 	if root == nil {
+		return NodeInfo{}
+	}
+	leftRet := FindNonKBalancedNode(root.left, k)
+	if leftRet.node != nil {
+		return leftRet
+	}
+
+	rightRet := FindNonKBalancedNode(root.right, k)
+	if rightRet.node != nil {
+		return rightRet
+	}
+
+	newSize := leftRet.size + rightRet.size + 1
+	if AbsInt(leftRet.size, rightRet.size) > k {
+		return NodeInfo{
+			node: root,
+			size: newSize,
+		}
+	} else {
 		return NodeInfo{
 			node: nil,
+			size: newSize,
 		}
 	}
-	return NodeInfo{}
+
+}
+
+func AbsInt(i, j int) int {
+	if i > j {
+		return i - j
+	}
+	return j - i
 }
 
 func main() {
--- chapter_10/10.1_TestIfABinaryTreeIsHeight-Balanced/variant/FindNotK-BalancedNode/main_test.go
+++ chapter_10/10.1_TestIfABinaryTreeIsHeight-Balanced/variant/FindNotK-BalancedNode/main_test.go
@@ -5,20 +5,355 @@ import "testing"
 var tests = []struct {
 	root    *TreeNode //the root node of test tree
 	kValue  int       //the wanted k-banlanced degree
-	expNode *TreeNode //the expected node pointer
+	expNode string    //the expected node name
 }{
-	{
+	{ //0
 		root:    nil,
 		kValue:  1,
-		expNode: nil,
+		expNode: "",
+	},
+	{ //1
+		root: &TreeNode{
+			name: "root",
+		},
+		kValue:  1,
+		expNode: "",
+	},
+	{ //2
+		root: &TreeNode{
+			name: "root",
+			left: &TreeNode{
+				name: "left",
+			},
+		},
+		kValue:  0,
+		expNode: "root",
+	},
+	{ //3
+		root: &TreeNode{
+			name: "root",
+			left: &TreeNode{
+				name: "left",
+			},
+		},
+		kValue:  1,
+		expNode: "",
+	},
+	{ //4
+		root: &TreeNode{
+			name: "root",
+			right: &TreeNode{
+				name: "right",
+			},
+		},
+		kValue:  0,
+		expNode: "root",
+	},
+	{ //5
+		root: &TreeNode{
+			name: "root",
+			left: &TreeNode{
+				name: "leftA",
+				left: &TreeNode{
+					name: "leftB",
+				},
+			},
+		},
+		kValue:  1,
+		expNode: "root",
+	},
+	{ //6
+		root: &TreeNode{
+			name: "root",
+			right: &TreeNode{
+				name: "right",
+				left: &TreeNode{
+					name: "left",
+				},
+			},
+		},
+		kValue:  1,
+		expNode: "root",
+	},
+	{ //7
+		root: &TreeNode{
+			name: "root",
+			left: &TreeNode{
+				name: "left",
+			},
+			right: &TreeNode{
+				name: "right",
+			},
+		},
+		kValue:  0,
+		expNode: "",
+	},
+	{ //8
+		root: &TreeNode{
+			name: "root",
+			left: &TreeNode{
+				name: "leftA",
+			},
+			right: &TreeNode{
+				name: "right",
+				left: &TreeNode{
+					name: "leftB",
+				},
+			},
+		},
+		kValue:  0,
+		expNode: "right",
+	},
+	{ //9
+		root: &TreeNode{
+			name: "root",
+			left: &TreeNode{
+				name: "leftA",
+				left: &TreeNode{
+					name: "leftB",
+				},
+			},
+			right: &TreeNode{
+				name: "rightA",
+				right: &TreeNode{
+					name: "rightB",
+				},
+			},
+		},
+		kValue:  0,
+		expNode: "leftA",
+	},
+	{ //10
+		root: &TreeNode{
+			name: "root",
+			left: &TreeNode{
+				name: "leftA",
+				left: &TreeNode{
+					name: "leftB",
+					right: &TreeNode{
+						name: "rightC",
+					},
+				},
+			},
+			right: &TreeNode{
+				name: "rightA",
+				right: &TreeNode{
+					name: "rightB",
+				},
+			},
+		},
+		kValue:  0,
+		expNode: "leftB",
+	},
+	{ //11
+		root: &TreeNode{
+			name: "root",
+			left: &TreeNode{
+				name: "leftA",
+				left: &TreeNode{
+					name: "leftB",
+					left: &TreeNode{
+						name: "leftC",
+						left: &TreeNode{
+							name: "leftD",
+						},
+					},
+				},
+			},
+		},
+		kValue:  2,
+		expNode: "leftA",
+	},
+	{ //12
+		root: &TreeNode{
+			name: "root",
+			left: &TreeNode{
+				name: "leftA",
+				left: &TreeNode{
+					name: "leftB",
+					left: &TreeNode{
+						name: "leftC",
+						left: &TreeNode{
+							name: "leftD",
+						},
+					},
+					right: &TreeNode{
+						name: "rightB",
+					},
+				},
+				right: &TreeNode{
+					name: "rightA",
+					left: &TreeNode{
+						name: "leftE",
+					},
+				},
+			},
+		},
+		kValue:  2,
+		expNode: "root",
+	},
+	{ //13
+		root: &TreeNode{
+			name: "root",
+			left: &TreeNode{
+				name: "leftA",
+				right: &TreeNode{
+					name: "rightA",
+					left: &TreeNode{
+						name: "leftB",
+						right: &TreeNode{
+							name: "rightB",
+							left: &TreeNode{
+								name: "leftC",
+								right: &TreeNode{
+									name: "rightC",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		kValue:  0,
+		expNode: "leftC",
+	},
+	{ //14
+		root: &TreeNode{
+			name: "root",
+			left: &TreeNode{
+				name: "leftA",
+				right: &TreeNode{
+					name: "rightA",
+					left: &TreeNode{
+						name: "leftB",
+						right: &TreeNode{
+							name: "rightB",
+							left: &TreeNode{
+								name: "leftC",
+								right: &TreeNode{
+									name: "rightC",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		kValue:  1,
+		expNode: "rightB",
+	},
+	{ //15
+		root: &TreeNode{
+			name: "root",
+			left: &TreeNode{
+				name: "leftA",
+				right: &TreeNode{
+					name: "rightA",
+					left: &TreeNode{
+						name: "leftB",
+						right: &TreeNode{
+							name: "rightB",
+							left: &TreeNode{
+								name: "leftC",
+								right: &TreeNode{
+									name: "rightC",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		kValue:  2,
+		expNode: "leftB",
+	},
+	{ //16
+		root: &TreeNode{
+			name: "root",
+			left: &TreeNode{
+				name: "leftA",
+				right: &TreeNode{
+					name: "rightA",
+					left: &TreeNode{
+						name: "leftB",
+						right: &TreeNode{
+							name: "rightB",
+							left: &TreeNode{
+								name: "leftC",
+								right: &TreeNode{
+									name: "rightC",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		kValue:  3,
+		expNode: "rightA",
+	},
+	{ //17
+		root: &TreeNode{
+			name: "root",
+			left: &TreeNode{
+				name: "leftA",
+				right: &TreeNode{
+					name: "rightA",
+					left: &TreeNode{
+						name: "leftB",
+						right: &TreeNode{
+							name: "rightB",
+							left: &TreeNode{
+								name: "leftC",
+								right: &TreeNode{
+									name: "rightC",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		kValue:  4,
+		expNode: "leftA",
+	},
+	{ //18
+		root: &TreeNode{
+			name: "root",
+			left: &TreeNode{
+				name: "leftA",
+				right: &TreeNode{
+					name: "rightA",
+					left: &TreeNode{
+						name: "leftB",
+						right: &TreeNode{
+							name: "rightB",
+							left: &TreeNode{
+								name: "leftC",
+								right: &TreeNode{
+									name: "rightC",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		kValue:  5,
+		expNode: "root",
 	},
 }
 
 func Test_FindNonKBalancedNode(t *testing.T) {
 	for idx, tt := range tests {
 		ret := FindNonKBalancedNode(tt.root, tt.kValue)
-		if ret.node != tt.expNode {
-			t.Errorf("[%d] expected=%v, got=%v", idx, tt.expNode, ret.node)
+		if ret.node == nil {
+			if tt.expNode != "" {
+				t.Errorf("[%d] expected=%s, got=", idx, tt.expNode)
+			}
+		} else if ret.node.name != tt.expNode {
+			t.Errorf("[%d] expected=%s, got=%s",
+				idx, tt.expNode, ret.node.name)
 		}
 	}
 }
